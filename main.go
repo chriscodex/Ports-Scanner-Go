a96e4f25de4ac3c319d580a57ed7e051b33ef890
Christian <3003html@gmail.com>
2022-07-19 22:09:31 -0500

    waitGroup added

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 1; i <= 65535; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port %d is open\n", i)
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait()
 }
