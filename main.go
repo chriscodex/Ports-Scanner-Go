a0eb10038f26b8bcbedcf5a98f140326eb235d88
Christian <3003html@gmail.com>
2022-07-19 22:19:00 -0500

    Flag function implemented

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan")
+
 func main() {
+	flag.Parse()
 	var wg sync.WaitGroup
 	for i := 1; i <= 65535; i++ {
 		wg.Add(1)
 		go func(port int) {
 			defer wg.Done()
-			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", port))
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
 			if err != nil {
 				return
 			}
